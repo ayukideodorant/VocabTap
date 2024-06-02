4aef1220d7b772c2f09645deba7d5ae3e462a341
ayukideodorant <tsubosseinto@outlook.jp>
2024-06-02 18:00:52 +0900

    データベースを作成した
    Co-authored-by: kin-downey <kin-downey@users.noreply.github.com>

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,20 @@
 from django.http.response import JsonResponse
+
+from . import models
+
 import random
 # Create your views here.
 def test(req):
     print("777")
-    users = ["taro", "yuko", "ayuki", "kin-downey"]
-    user_name = random.choice(users)
-    return JsonResponse({"message": user_name})
+    first_names = ['taro', 'jiro', 'saburo', 'shiro']
+    last_names = ['suzuki', 'tanaka', 'yamada', 'kawasaki']
+    cities = ['kyoto', 'seikacho', 'uji', 'kameoka']
+    first_name = random.choice(first_names)
+    last_name = random.choice(last_names)
+    city = random.choice(cities)
+    # リクエストの中身を取得
+    address = req.GET.get('address', 'Higasi kujo')
+    # インスタンスの作成
+    person = models.Person(first_name=first_name, last_name=last_name, city=city, address=address)
+    person.save()
+    return JsonResponse({"message": "created person", "first_name": first_name, "last_name": last_name, "city": city, "address": address})

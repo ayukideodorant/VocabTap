db4691ea4edb19cd2c97d5c7b3b9d9ec44403385
ayukideodorant <tsubosseinto@outlook.jp>
2024-06-04 22:39:31 +0900

    テーブルを作成した。
    Co-authored-by: kin-downey <kin-downey@users.noreply.github.com>

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,3 +6,43 @@ class Person(models.Model):
     first_name = models.CharField(max_length=30)
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=10)
+
+class TypRecord(models.Model):
+    frequent = models.IntegerField()
+    faild_count = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+
+class ProgramLanguage(models.Model):
+    language_name = models.CharField(max_length=20)
+
+class Word(models.Model):
+    alphabet = models.CharField(max_length=20)
+    program_language = models.ForeignKey(ProgramLanguage, on_delete=models.CASCADE)
+
+class FaildWord(models.Model):
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    record = models.ForeignKey(TypRecord, on_delete=models.CASCADE)
+
+
+# # 名前がayukiのユーザーを取得
+# user = User.filter(name='ayuki').first()
+# print(user.name) # ayuki
+
+
+
+# user = select * from User where name='ayuki' limit 1;
+# user_name = user[1]
+# print(user_name)
+
+'''
+Python
+
+user_name : スネークケース : 変数、関数
+UserName : アッパーキャメルケース : クラス名
+userName : ロウアーキャメルケース : 使わない
+'''
